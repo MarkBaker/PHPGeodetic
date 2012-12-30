c9d3f764b3938d3828e55358438147729d8ae6e8
Mark Baker <mark@lange.demon.co.uk>
2012-12-30 23:55:02 +0000

    Perimeter and Areas for Regions (initial version)

--- classes/Geodetic_Region.php
+++ classes/Geodetic_Region.php
@@ -63,6 +63,143 @@ class Geodetic_Region
         return $this->_perimeterPoints;
     }
 
+    private static function _haversineAdjust($x)
+    {
+        return (1.0 - cos($x)) / 2.0;
+    }
+
+    /**
+     *  Get the Planar Area of this region
+     *
+     *  @return    Geodetic_Area    The planar area of this region
+     */
+    public function getAreaPlanar(Geodetic_ReferenceEllipsoid $ellipsoid = NULL)
+    {
+        $pointCount = count($this->_perimeterPoints);
+        if ($pointCount == 0)
+            return new Geodetic_Area();
+
+        if (is_null($ellipsoid)) {
+            $ellipsoid = new Geodetic_ReferenceEllipsoid(Geodetic_ReferenceEllipsoid::WGS_1984);
+        }
+        $radius = $ellipsoid->getSemiMajorAxis();
+
+        $area = 0;
+        for($j = 0; $j < $pointCount; ++$j) {
+            $k = $j + 1;
+            if ($j == 0) {
+                $lambda1 = $this->_perimeterPoints[$j]->getLongitude()->getValue(Geodetic_Angle::RADIANS);
+                $beta1 = $this->_perimeterPoints[$j]->getLatitude()->getValue(Geodetic_Angle::RADIANS);
+                $lambda2 = $this->_perimeterPoints[$k]->getLongitude()->getValue(Geodetic_Angle::RADIANS);
+                $beta2 = $this->_perimeterPoints[$k]->getLatitude()->getValue(Geodetic_Angle::RADIANS);
+                $cosB1 = cos($beta1);
+                $cosB2 = cos($beta2);
+            } else {
+                $k = ($j+1) % $pointCount;
+                $lambda1 = $lambda2;
+                $beta1 = $beta2;
+                $lambda2 = $this->_perimeterPoints[$k]->getLongitude()->getValue(Geodetic_Angle::RADIANS);
+                $beta2 = $this->_perimeterPoints[$k]->getLatitude()->getValue(Geodetic_Angle::RADIANS);
+                $cosB1 = $cosB2;
+                $cosB2 = cos($beta2);
+            }
+            if ($lambda1 != $lambda2) {
+                $haversine = self::_haversineAdjust($beta2 - $beta1) +
+                    $cosB1 * $cosB2 * self::_haversineAdjust($lambda2 - $lambda1);
+                $a = 2 * asin(sqrt($haversine));
+                $b = M_PI / 2 - $beta2;
+                $c = M_PI / 2 - $beta1;
+                $s = 0.5 * ($a + $b + $c);
+                $t = tan($s / 2) * tan(($s - $a) / 2) * tan(($s - $b) / 2) * tan(($s - $c) / 2);
+
+                $excess = abs(4 * atan(sqrt(abs($t))));
+
+                if ($lambda2 < $lambda1) {
+                    $excess = -$excess;
+                }
+                $area += $excess;
+            }
+        }
+
+        return new Geodetic_Area(
+            abs($area) * $radius * $radius
+        );
+    }
+
+    /**
+     *  Get the Area of this region
+     *
+     *  @return    Geodetic_Area    The area of this region
+     */
+    public function getArea(Geodetic_ReferenceEllipsoid $ellipsoid = NULL)
+    {
+        $pointCount = count($this->_perimeterPoints);
+        if ($pointCount == 0)
+            return new Geodetic_Area();
+
+        if (is_null($ellipsoid)) {
+            $ellipsoid = new Geodetic_ReferenceEllipsoid(Geodetic_ReferenceEllipsoid::WGS_1984);
+        }
+        $radius1 = $ellipsoid->getSemiMajorAxis();
+        $radius2 = $ellipsoid->getSemiMinorAxis();
+
+        $area = 0;
+        for($i = 0; $i < $pointCount; ++$i) {
+            $j = ($i+1) % $pointCount;
+            $h = ($i-1) % $pointCount;
+            if ($h < 0)
+                $h += $pointCount;
+            $area += ($this->_perimeterPoints[$j]->getLongitude()->getValue(Geodetic_Angle::RADIANS) -
+                      $this->_perimeterPoints[$h]->getLongitude()->getValue(Geodetic_Angle::RADIANS)) *
+                     sin($this->_perimeterPoints[$i]->getLatitude()->getValue(Geodetic_Angle::RADIANS));
+        }
+
+        return new Geodetic_Area(
+            abs($area * $radius1 * $radius2 / 2)
+        );
+    }
+
+    /**
+     *  Get the length along the Perimeter for this region
+     *
+     *  @param     Geodetic_ReferenceEllipsoid    $ellipsoid       If left blank or NULL, then WGS 84 Ellipsoid will be used
+     *  @param     boolean                        $useHaversine    If true, then the Haversine formula will be used for the
+     *                                                                 calculation, otherwise the more accurate Vincenty
+     *                                                                 formula will be used instead.
+     *  @return    Geodetic_Distance    The length along the perimeter for this region
+     */
+    public function getPerimeter(Geodetic_ReferenceEllipsoid $ellipsoid = NULL,
+                                 $useHaversine = FALSE)
+    {
+        $pointCount = count($this->_perimeterPoints);
+        if ($pointCount == 0)
+            return new Geodetic_Area();
+
+        if (is_null($ellipsoid)) {
+            $ellipsoid = new Geodetic_ReferenceEllipsoid(Geodetic_ReferenceEllipsoid::WGS_1984);
+        }
+
+        $distance = 0;
+        for($i = 0; $i < $pointCount; ++$i) {
+            $j = ($i+1) % $pointCount;
+            if ($useHaversine) {
+                $distance += $this->_perimeterPoints[$i]->getDistanceHaversine(
+                    $this->_perimeterPoints[$j],
+                    $ellipsoid
+                )->getValue();
+            } else {
+                $distance += $this->_perimeterPoints[$i]->getDistanceVincenty(
+                    $this->_perimeterPoints[$j],
+                    $ellipsoid
+                )->getValue();
+            }
+        }
+
+        return new Geodetic_Distance(
+            $distance
+        );
+    }
+
     /**
      *  Identify whether a specified Latitude/Longitude falls within the bounds of this region
      *
